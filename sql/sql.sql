c11234e4f5fda851fd5a77e8b60d8d84fc3ba707
ma <501847822@qq.com>
2024-06-30 23:52:11 +0800

    add 数据库建表语句

---
+++ sql/sql.sql
@@ -0,0 +1,51 @@
+create database if not exists access_control;
+use access_control;
+-- 创建管理员表
+drop table if exists admin;
+CREATE TABLE if not exists admin (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '管理员ID',
+    userAccount VARCHAR(50) NOT NULL UNIQUE COMMENT '用户账号',
+    userPassword VARCHAR(255) NOT NULL COMMENT '用户密码',
+    username VARCHAR(100) COMMENT '用户姓名',
+    gender TINYINT DEFAULT  0 COMMENT '用户性别: 0-男 | 1-女',
+    age INT COMMENT '用户年龄',
+    position VARCHAR(100) COMMENT '岗位',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间'
+    ) COMMENT '管理员表';
+
+-- 创建用户表
+drop table if exists user;
+CREATE TABLE if not exists user (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '用户id',
+    username VARCHAR(100) NOT NULL UNIQUE COMMENT '用户姓名',
+    gender TINYINT DEFAULT  0 COMMENT '用户性别: 0-男 | 1-女',
+    age INT COMMENT '用户年龄',
+    position VARCHAR(100) COMMENT '岗位',
+    createTime datetime default CURRENT_TIMESTAMP comment '注册时间',
+    updateTime datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间'
+    ) COMMENT '用户表';
+
+-- 创建出入表
+drop table if exists access;
+CREATE TABLE if not exists access (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT '出入表id',
+    userId BIGINT NOT NULL COMMENT '用户id',
+    checkInTime DATETIME DEFAULT CURRENT_TIMESTAMP null,
+    checkInStatus  TINYINT DEFAULT 0 COMMENT '0-签到失败 | 1-签到成功',
+    checkInImage TEXT COMMENT '签到状态图片',
+    checkIutTime DATETIME DEFAULT CURRENT_TIMESTAMP null,
+    checkOutStatus TINYINT DEFAULT 0 COMMENT '0-签退失败 | 1-签退成功',
+    checkOutImage TEXT COMMENT '签退状态图片'
+) COMMENT '出入表';
+
+-- 创建日志表
+drop table if exists log;
+CREATE TABLE if not exists log (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '日志id',
+    userId BIGINT COMMENT '用户ID',
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP null,
+    checkInStatus  TINYINT DEFAULT 0 COMMENT '0-签到失败 | 1-签到成功',
+    checkOutStatus TINYINT DEFAULT 0 COMMENT '0-签退失败 | 1-签退成功',
+    PRIMARY KEY (userId, timestamp)
+) COMMENT '日志表';

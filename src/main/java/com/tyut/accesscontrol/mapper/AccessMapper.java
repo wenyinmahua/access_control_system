f25102a9434438099ee5ab1842ad3ee0d3097b7e
ma <501847822@qq.com>
2024-07-03 01:32:03 +0800

    update 出入表service层更新

---
+++ src/main/java/com/tyut/accesscontrol/mapper/AccessMapper.java
@@ -0,0 +1,18 @@
+package com.tyut.accesscontrol.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tyut.accesscontrol.model.entity.Access;
+
+/**
+* @author mahua
+* @description 针对表【access(出入表)】的数据库操作Mapper
+* @createDate 2024-07-03 01:25:43
+* @Entity com.tyut.accesscontrol.model.entity.Access.Access
+*/
+public interface AccessMapper extends BaseMapper<Access> {
+
+}
+
+
+
+
---
+++ src/main/java/com/tyut/accesscontrol/service/AccessService.java
@@ -0,0 +1,13 @@
+package com.tyut.accesscontrol.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tyut.accesscontrol.model.entity.Access;
+
+/**
+* @author mahua
+* @description 针对表【access(出入表)】的数据库操作Service
+* @createDate 2024-07-03 01:25:43
+*/
+public interface AccessService extends IService<Access.Access> {
+
+}

5b9353dd1616c40571fba3292d1ae99859ec7be5
ma <501847822@qq.com>
2024-07-03 09:12:15 +0800

    fix 出入表service层修复

--- src/main/java/com/tyut/accesscontrol/service/AccessService.java
+++ src/main/java/com/tyut/accesscontrol/service/AccessService.java
@@ -8,6 +8,6 @@ import com.tyut.accesscontrol.model.entity.Access;
 * @description 针对表【access(出入表)】的数据库操作Service
 * @createDate 2024-07-03 01:25:43
 */
-public interface AccessService extends IService<Access.Access> {
+public interface AccessService extends IService<Access> {
 
 }

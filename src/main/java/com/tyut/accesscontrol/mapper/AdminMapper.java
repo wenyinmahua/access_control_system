3b9f5999558c6107eab7837d9dcda37b7c384ffc
ma <501847822@qq.com>
2024-07-03 09:20:20 +0800

    fix mapper层修复

--- src/main/java/com/tyut/accesscontrol/mapper/AdminMapper.java
+++ src/main/java/com/tyut/accesscontrol/mapper/AdminMapper.java
@@ -1,27 +1,16 @@
 package com.tyut.accesscontrol.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tyut.accesscontrol.model.entity.Access;
 import com.tyut.accesscontrol.model.entity.Admin;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
-public interface AdminMapper {
+public interface AdminMapper extends BaseMapper<Admin> {
 
-    @Select("SELECT * FROM admin WHERE userAccount = #{userAccount}")
-    Admin findByUserAccount(@Param("userAccount") String userAccount);
 
-    @Insert("INSERT INTO admin (userAccount, userPassword, username, gender, age, position) VALUES (#{userAccount}, #{userPassword}, #{username}, #{gender}, #{age}, #{position})")
-    int insert(Admin admin);
-
-    @Update("UPDATE admin SET userPassword=#{newPassword}, username=#{newUsername}, gender=#{newGender}, age=#{newAge}, position=#{newPosition} WHERE id=#{id}")
-    void update(Admin admin);
-
-    @Delete("DELETE FROM admin WHERE id = #{id}")
-    void deleteById(@Param("id") Long id);
-
-    @Select("SELECT * FROM admin")
-    List<Admin> selectAll();
 }
 
 
